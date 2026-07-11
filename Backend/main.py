1ef591812f8e3f051201d174c94b90aa89a0b08b
RahulPatha001 <rahul.patha29@gmail.com>
2026-07-11 11:59:40 +0530

    ci: added inngest and verified connection

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from backend!")
+import logging
+from fastapi import FastAPI
+import inngest
+import inngest.fast_api
+import uuid
+from dotenv import load_dotenv
+import os
+import datetime
+from inngest.experimental import ai
 
+load_dotenv()
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+inngest_client = inngest.Inngest(
+    app_id = "rag_app",
+    logger = logging.getLogger("uvicorn"),
+    is_production= False,
+    serializer=inngest.PydanticSerializer()
+)
+
+@inngest_client.create_function(
+    fn_id="RAG_Ingest PDF",
+    trigger= inngest.TriggerEvent(event="rag/inngest_pdf")
+)
+async def rag_inngest_pdf(ctx: inngest.Context):
+    return {"hello":"world"}
+
+inngest.fast_api.serve(app, inngest_client,[rag_inngest_pdf])
