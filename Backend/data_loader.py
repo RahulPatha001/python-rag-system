dbf1afac2345c9e0bd7fb32069ea43d4b1809ad0
RahulPatha001 <rahul.patha29@gmail.com>
2026-07-26 08:53:39 +0530

    CI: added methods related to data loading

--- Backend/data_loader.py
+++ Backend/data_loader.py
@@ -1,6 +1,30 @@
+import os
 from openai import OpenAI
 from llama_index.readers.file import PDFReader
 from llama_index.core.node_parser import SentenceSplitter
 from dotenv import load_dotenv
 
 load_dotenv()
+
+client = OpenAI(api_key=os.getenv("API_KEY"))
+EMBED_MODEL = "text-embedding-3-large"
+EMBED_DIM = 3072
+
+spilitter = SentenceSplitter(chunk_size=1000, chunk_overlap=200)
+
+def load_and_chunk_pdf (file_path):
+    pdf_reader = PDFReader()
+    documents = pdf_reader.load_data(file=file_path)
+    texts = [d.text for d in documents if getattr(d, 'text', None)]
+    chunks = []
+    for text in texts:
+        chunks.extend(spilitter.split_text(text))
+    return chunks
+
+def embed_texts(texts: list[str]) -> list[list[float]]:
+    response = client.embeddings.create(
+        model=EMBED_MODEL,
+        input=texts
+    )
+    embeddings = [item.embedding for item in response.data]
+    return embeddings
